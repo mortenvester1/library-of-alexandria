de479a85f693f5865d21d06bb40aa273b245435a
Morten Pedersen <mortenvester1@gmail.com>
2025-03-22 10:21:51 -0400

    Update bootstrap_dotfiles.bash

--- scripts/bootstrap_dotfiles.bash
+++ scripts/bootstrap_dotfiles.bash
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# Install home brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Install stow
-/opt/homebrew/bin/brew install stow
-alias stow=/opt/homebrew/bin/stow
-
 cd dotfiles
 if [[ "${1}" = pi ]]
 then
-  sudo apt install -y stow
   stow -t ${HOME} zshpi
 else
-  brew install stow
   stow -t ${HOME} zsh gnupg zed
 fi
 
